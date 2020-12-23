fec8eead4f95aab63db4c3614ceb4074ae17ca42
sushil70 <sushil29121997@gmail.com>
2020-12-23 22:19:24 +0530

    second

--- first/src/App.js
+++ first/src/App.js
@@ -4,7 +4,7 @@ import "./App.css"
 function App() {
     return (
         <div className="App">
-            <h1>Hello Sushil kumar</h1>
+            <h1>Hello Sushil kumar ji</h1>
         </div>
     )
 }
